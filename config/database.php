875aefd74ff94608ad39f266e9dcb8b347c04f66
MuQi97 <muq971118@gmail.com>
2020-02-11 23:15:43 +0800

    Database

--- config/database.php
+++ config/database.php
@@ -66,10 +66,9 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => $db_config['host'],
-            'port' => env('DB_PORT', '5432'),
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'port'     => env('DB_PORT', '5432'),
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
