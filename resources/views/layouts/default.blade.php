7e0ca991c602bd074d3733c1a4182228373da76b
MuQi97 <muq971118@gmail.com>
2020-02-11 19:01:05 +0800

    login and nav

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,21 +2,22 @@
 <html>
 
 <head>
-  <title>@yield('title', 'MuQ App') - MuQ Demo</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	<title>@yield('title', 'MuQ App') - MuQ Demo</title>
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
 
-  @include('layouts._header')
+	@include('layouts._header')
 
-  <div class="container">
-    <div class="offset-md-1 col-md-10">
-      @include('shared._messages')
-      @yield('content')
-      @include('layouts._footer')
-    </div>
-  </div>
+	<div class="container">
+		<div class="offset-md-1 col-md-10">
+			@include('shared._messages')
+			@yield('content')
+			@include('layouts._footer')
+		</div>
+	</div>
+	<script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
